4eb3bbaca4c537463dca6ae20dfadc7c88708f0d
scottgirling <scottgirling0812@gmail.com>
2025-04-09 16:33:14 +0100

    amend pagination on article list page

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,7 +4,7 @@ import { Loading } from "./Loading";
 import { fetchArticles } from "../utils/api";
 import '../ArticleList.css'
 import { ErrorPage } from "./ErrorPage";
-import { Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { Box, FormControl, InputLabel, Select, MenuItem, Stack, Pagination } from "@mui/material";
 
 export const ArticleList = () => {
     const { topic } = useParams();
@@ -51,6 +51,16 @@ export const ArticleList = () => {
             setErrorFindingTopic(error.response.data.msg);
         });
     }, [topic, pageQuery, sortByQuery, orderQuery]);
+
+    const handleChange = (event) => {
+        if (event.target.innerHTML === "<path d=\"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z\"></path>") {
+            setPage(Number(pageQuery) + 1);
+        } else if (event.target.innerHTML === "<path d=\"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z\"></path>") {
+            setPage(Number(pageQuery) - 1);
+        } else {
+            setPage(Number(event.target.innerText));
+        }
+    } 
 
     if (errorFindingTopic) {
         return (
@@ -179,23 +189,16 @@ export const ArticleList = () => {
                         })}
                     </ul>
                 </main>
-                
-                <div className="pages">
-                    <button type="submit" onClick={() => {
-                        {pageQuery > 1 ? setPage(Number(pageQuery) - 1) : null};
-                    }}><i className="fa-solid fa-angle-left"></i> Previous
-                    </button>
-                    <p>{pageQuery || 1}</p>
-                    <button type="submit" onClick={() => {
-                            {!pageQuery ? (
-                                setPage(Number(pageQuery) + 2) 
-                            ) : (
-                                setPage(Number(pageQuery) + 1)
-                            )};
-                        }
-                    }>Next <i className="fa-solid fa-angle-right"></i>
-                    </button>
-                </div>
+
+                <Stack>
+                   <Pagination
+                       count={4}
+                       page={Number(pageQuery) || 1}
+                       shape="rounded"
+                       onChange={(event) => handleChange(event)}
+                       sx={{ mt: 1, width: "65vw", padding: ".5rem 1rem", "& > *": {  justifyContent: "center" } }}
+                    />
+               </Stack>
             </>}
         </>
     );
